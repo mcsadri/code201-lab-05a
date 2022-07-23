de2111586e3710f15f976a63a4a09c5865681d24
Manuch Sadri <colonquebang@hotmail.com>
2022-07-23 16:44:27 -0700

    completed lab 05a problem #5: function multiplyArray()

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-    let sumArrayABC = sum(  sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+    let sumArrayABC = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
     let concatSumArrayABC = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumArrayABC} is their sum.`;
     let arraySumABC = [sumArrayABC, concatSumArrayABC];
     return arraySumABC;
@@ -93,7 +93,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -109,11 +109,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    let productArrayABC = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+    let concatProductArrayABC = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${productArrayABC}.`;
+    let arrayProductArrayABC = [productArrayABC, concatProductArrayABC];
+    return arrayProductArrayABC;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
