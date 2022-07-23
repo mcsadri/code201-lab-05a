a5fa2bff3eed71388f2a9faa88d8f7647d51354f
Manuch Sadri <colonquebang@hotmail.com>
2022-07-23 16:23:39 -0700

    completed lab 05a problem #4: function sumArray()

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -85,12 +85,15 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    let sumArrayABC = sum(  sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+    let concatSumArrayABC = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumArrayABC} is their sum.`;
+    let arraySumABC = [sumArrayABC, concatSumArrayABC];
+    return arraySumABC;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
